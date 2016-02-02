563154dfb638cc71480f35cb420d82935918537f
VincentJiang <jxs471494539@gmail.com>
2016-02-02 21:44:50 +0800

    remove dotfile

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,7 +39,7 @@ task :environment do
   # invoke :'rbenv:load'
 
   # For those using RVM, use this to load an RVM version@gemset.
-  invoke :'rvm:use[ruby-2.2.2@vrails]'
+  invoke :'rvm:use[ruby-2.2.2]'
 end
 
 # Put any custom mkdir's in here for when `mina setup` is ran.
